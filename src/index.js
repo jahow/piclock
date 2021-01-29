cc6489bf8a984cfad0508fe57f8fe61e37d7b3e4
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-01-29 19:47:50 +0100

    redo layout with placeholders

--- src/index.js
+++ src/index.js
@@ -5,17 +5,28 @@ import { clockWidget } from './modules/widgets/clock';
 import { textWidget } from './modules/widgets/text';
 import { graphWidget } from './modules/widgets/graph';
 
-addWidget(clockWidget(2, 2, 1));
+addWidget(clockWidget(7, 2, 1));
 
 // addWidget(testWidget(52, 45, 20, 2));
 
-addWidget(textWidget(56, 5, 'lun.', 2));
-addWidget(textWidget(56, 12, '04-02', 2));
+addWidget(textWidget(11, 19, 'lun. 04-02', 3));
 
-addWidget(graphWidget(2, 23, 20, 31, 2, 3));
-addWidget(textWidget(26, 20, 't°', 3));
-addWidget(textWidget(26, 27, '18.2', 1));
+// phase A: temp graph
+addWidget(graphWidget(2, 23, 26, 37, 2, 3));
+addWidget(textWidget(26, 26, 't°', 3));
+addWidget(textWidget(26, 33, '18.2', 2));
 
-addWidget(graphWidget(2, 23, 34, 45, 2, 3));
-addWidget(textWidget(26, 34, 'co2', 3));
-addWidget(textWidget(26, 41, '421', 1));
+addWidget(textWidget(50, 26, 'co2', 3));
+addWidget(textWidget(45, 33, '2420', 2));
+
+// phase b: weather forecast
+// addWidget(textWidget(5, 26, 'o', 2));
+// addWidget(textWidget(2, 33, '32', 2));
+// addWidget(textWidget(18, 26, 'o', 2));
+// addWidget(textWidget(15, 33, '32', 2));
+// addWidget(textWidget(31, 26, 'o', 2));
+// addWidget(textWidget(28, 33, '32', 2));
+// addWidget(textWidget(44, 26, 'o', 2));
+// addWidget(textWidget(41, 33, '32', 2));
+// addWidget(textWidget(57, 26, 'o', 2));
+// addWidget(textWidget(54, 33, '32', 2));
